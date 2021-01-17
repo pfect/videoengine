d95ae39648884bd6ec59e40a3bf9e081b068ef79
Pasi Patama <github@patama.net>
2021-01-17 07:46:55 +0200

    Removed randomstring function.

--- src/videosend.c
+++ src/videosend.c
@@ -30,20 +30,6 @@
 #include <gst/gst.h>
 #define MAXCHAR 1000
 
-static char *rand_string(char *str, size_t size)
-{
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJK...";
-    if (size) {
-        --size;
-        for (size_t n = 0; n < size; n++) {
-            int key = rand() % (int) (sizeof charset - 1);
-            str[n] = charset[key];
-        }
-        str[size] = '\0';
-    }
-    return str;
-}
-
 /* Take vitals from oximeter provided data */
 static char readvitals(char *str)
 {
